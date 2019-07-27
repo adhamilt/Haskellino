d8ee1bab5e5054663573806fee9e848b7910a2d6
Alex <protocorbulo@gmail.com>
2019-07-27 13:13:50 -0600

    more parser work

--- src/Parser.hs
+++ src/Parser.hs
@@ -17,19 +17,67 @@ languageDef =
            , Token.commentLine     = "//"
            , Token.identStart      = letter
            , Token.identLetter     = alphaNum
-           , Token.reservedNames   = [ "if"
-                                     , "then"
-                                     , "else"
-                                     , "while"
-                                     , "do"
-                                     , "skip"
-                                     , "true"
-                                     , "false"
-                                     , "not"
-                                     , "and"
-                                     , "or"
+           , Token.reservedNames   = [ "Integer"
                                      ]
-           , Token.reservedOpNames = ["+", "-", "*", "/", ":="
-                                     , "<", ">", "and", "or", "not"
+           , Token.reservedOpNames = ["+", "-", "=","::", "->"
                                      ]
            }
+
+lexer :: Token.TokenParser ()
+lexer = Token.makeTokenParser languageDef
+
+
+identifier :: Parser String
+identifier = Token.identifier lexer
+
+reserved :: String -> Parser ()
+reserved = Token.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Token.reservedOp lexer
+
+parens :: Parser a -> Parser a
+parens = Token.parens lexer
+
+integer :: Parser Integer
+integer = Token.integer lexer
+
+whiteSpace :: Parser ()
+whiteSpace = Token.whiteSpace lexer
+
+
+
+data Type = Typename String
+            deriving (Show)
+
+data ArithmeticBinaryOp = Add
+                        | Subtract
+                        | Multiply
+                        | Divide
+                          deriving (Show)
+
+data ArithmeticExpr = Var String
+                    | IntConst Integer
+                    | Neg ArithmeticExpr
+                    | ArithmeticBinary ArithmeticBinaryOp ArithmeticExpr ArithmeticExpr
+                      deriving (Show)
+
+data FunctionExpr = FunctionDef String [Type] Type
+                  | FunctionBody [String] ArithmeticExpr
+                    deriving (Show)
+
+data Statement = Seq [Statement]
+          | Assign String ArithmeticExpr
+          | Skip
+            deriving (Show)
+
+{-
+whileParser :: Parser Statement
+whileParser = whiteSpace >> statement
+
+
+statement :: Parser Statement
+statement = parens statement
+          <|> sequenceOfStatements
+
+-}
